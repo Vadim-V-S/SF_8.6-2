b781b44e5cc398530ce3ba25de84bb916cb8a398
Vadim-V-S <vaddimsav@gmail.com>
2021-06-03 12:38:12 +0500

    Добавьте файлы проекта.

---
+++ SF_8.6-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace SF_8._6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до папки");
+            string path = string.Format(@"{0}", Console.ReadLine());
+            Console.WriteLine("Размер папки {0} байт",GetFiles(path, 0));
+
+            Console.ReadKey();
+        }
+
+        static long GetFiles(string path, long size)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                DirectoryInfo[] dirArray = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+
+                foreach (FileInfo file in files)
+                    size += file.Length;
+                foreach (DirectoryInfo subFile in dirArray)
+                    GetFiles(subFile.FullName, size);
+                return size;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+    }
+}
